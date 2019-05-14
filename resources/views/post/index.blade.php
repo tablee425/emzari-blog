cd70be267c42bb841f558948f4b767e6526786c9
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-05-14 18:27:15 +0400

    add layout

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title')
+    Welcome Laravel Blog Tutorial
+@endsection
+
+@section('content')
+   <h1>Welcome to muva lab blog tutorial using laravel</h1>
+@endsection
