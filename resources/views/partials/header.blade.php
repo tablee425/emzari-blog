eb31ae043917e492fa7d84580b7720349a34d119
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-05-15 00:45:22 +0400

    display logged in user

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
-    <a class="navbar-brand" href="#">Emzari-Lab</a>
+    <a class="navbar-brand" href="http://localhost/home">Home</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,7 +9,7 @@
             <li class="nav-item dropdown">
 
                 <a class="nav-link dropdown-toggle"  id="dropdown09" data-toggle="dropdown"
-                   aria-haspopup="true" aria-expanded="false">User Account</a>
+                   aria-haspopup="true" aria-expanded="false">@guest User Account @else {{ Auth::user()->name }} @endguest</a>
                 <div class="dropdown-menu" aria-labelledby="dropdown09">
                     @guest
                     <a class="dropdown-item" href="{{ route('register') }}">Sign up</a>
