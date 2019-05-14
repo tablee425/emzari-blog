8e192743ccd559258f704aa93be8dc071f9b3985
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-05-14 20:15:01 +0400

    simple blog site

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
 
 class HomeController extends Controller
 {
@@ -19,10 +23,48 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home');
+        $posts = DB::table('users')->leftjoin('posts', 'users.id', '=', 'posts.author')->paginate(10);
+        return view('home', ['posts' => $posts]);
+    }
+
+    public function getPostForm() {
+        return view('post/post_form');
+    }
+
+    public function createPost(Request $request){
+        $post = Post::create(array(
+            'title' => Input::get('title'),
+            'description' => Input::get('description'),
+            'author' => Auth::user()->id
+        ));
+        return redirect()->route('home')->with('success', 'Post has been successfully added!');
+    }
+
+    public function getPost($id){
+        $post = Post::find($id);
+        return view('post/post_detail', ['post' => $post]);
+    }
+
+    public function editPost($id) {
+        $post = Post::find($id);
+        return view('post/edit_post', ['post' => $post]);
+    }
+
+    public function updatePost(Request $request, $id) {
+        $post = Post::find($id);
+        $post->title = $request->title;
+        $post->description = $request->description;
+        $post->save();
+        return redirect()->route('home')->with('success', 'Post has been updated successfully!');
+    }
+
+    public function deletePost($id) {
+        $post = Post::find($id);
+        $post->delete();
+        return redirect()->route('home')->with('success', 'Post has been deleted successfully!');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,64 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Post <span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Nav item again</a>
+                    </li>
+                </ul>
+            </nav>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+            <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
+                <h1>Posts
+                    <a href="{{ route('post.form') }}">
+                        <button type="button" class="btn btn-primary btn-sm">Create Post</button>
+                    </a>
+                </h1>
+                @if(Session::has('success'))
+                    <div class="row">
+                        <div class="col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3">
+                            <div id="message" class="alert alert-success">
+                                {{ Session::get('success') }}
+                            </div>
                         </div>
+                    </div>
+                @endif
+                <table class="table table-striped table-hover">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Learn more</th>
+                        <th>Created on</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @if($posts)
+                        @foreach($posts as $post)
+                            <tr>
+                                <th>{{ $loop->iteration }}</th>
+                                <td>{{ $post->title }}</td>
+                                <td>{{ $post->name }}</td>
+                                <td>
+                                    <a href="{{ route('post.detail', ['id' => $post->id]) }}">view more</a>
+                                </td>
+                                <td>{{ Carbon\Carbon::parse($post->created_at)->format('d-m-Y')  }}</td>
+                            </tr>
+                        @endforeach
+                    @else
+                        <p class="text-center text-primary">No Posts created Yet!</p>
                     @endif
-
-                    You are logged in!
-                </div>
-            </div>
+                </table><Br>
+            </main>
         </div>
     </div>
-</div>
 @endsection
