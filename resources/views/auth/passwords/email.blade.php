e58b07771236e3683dea6f522dff2d992b165ab7
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2019-05-31 19:54:17 +0400

    forgot password email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -14,8 +14,7 @@
                         </div>
                     @endif
 
-                    <!-- <form method="POST" action="{{ route('password.email') }}"> -->
-                    <form method="POST" action="">
+                    <form method="POST" action="{{ route('password.email') }}">
                         @csrf
 
                         <div class="form-group row">
